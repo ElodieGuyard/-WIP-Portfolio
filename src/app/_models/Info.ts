ed5b281790f69b1b229269678e4ea4130d1415d0
Elodie Guyard <elodieg29@gmail.com>
2024-12-27 09:30:52 +0100

    feat : contact page

---
+++ src/app/_models/Info.ts
@@ -0,0 +1,7 @@
+export class infos {
+
+static readonly CONTACT_EMAIl: string = "guyard.elodie@outlook.fr";
+static readonly CONTACT_LINKEDIN: string = "https://www.linkedin.com/in/%C3%A9lodie-guyard-219b01311/";
+static readonly CONTACT_GITHUB: string = "https://github.com/ElodieGuyard";
+
+}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { infos } from "../_models/Info";
+
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactEmail = infos.CONTACT_EMAIl; // Récupère l'email depuis la configuration _model/Infos
+  contactLinkedIn = infos.CONTACT_LINKEDIN; // Récupère le lien linkedIn depuis la configuration _model/Infos
+  contactGithub = infos.CONTACT_GITHUB; // Récupère le lien linkedIn depuis la configuration _model/Infos
 
 }
