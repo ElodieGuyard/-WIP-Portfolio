59ba5203484b9229c05687ee8c4a51cfcda8657a
Elodie Guyard <elodieg29@gmail.com>
2025-01-10 11:58:23 +0100

    Feat : new tab resume added & routeLink done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjetsComponent } from './projets/projets.component';
 import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
     { path: 'projets', component: ProjetsComponent},
+    { path: 'resume', component: ResumeComponent},
     { path: 'contact', component: ContactComponent},
     { path: 'projects/0', component: ProjectDetailsComponent},
     { path: 'projects/1', component: ProjectDetailsComponent},
