403dda2c09670b0a4c3c7e3cd7af669b6143b189
Guyard Elodie <guyard.elodie@outlook.fr>
2025-02-04 20:30:54 +0100

    WIPFeat : On the resume page, a button generate a PDF from the resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-resume',
@@ -8,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class ResumeComponent {
 
+  generatePDF(){
+    const elementToPrint = document.getElementById('contentToConvert');
+    
+    if (elementToPrint) {
+    html2canvas(elementToPrint, { scale: 2}).then((canva)=> {
+      const pdf = new jsPDF();
+      pdf.addImage(canva.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+
+      pdf.setProperties({
+        title:'Resume Elodie Guyard',
+        author:'ElodieGuyard'
+      });
+
+      pdf.setFontSize(11);
+      pdf.save('ResumeElodieGuyard.pdf');
+    })
+  }
+  }
 }
