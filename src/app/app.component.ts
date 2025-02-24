2d9f7b5add32b8b897c00ca21ae209b055b049e5
Guyard Elodie <guyard.elodie@outlook.fr>
2025-02-24 16:33:23 +0100

    Feat: Dark Mode btn and Light mode btn added with logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { NavComponent } from "./nav/nav.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,91 @@ import { NavComponent } from "./nav/nav.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Guyard Elodie Portfolio';
+  
+  isDarkModebtnActive : boolean = false;
+  isContrastModebtnActive : boolean = false;
+  
+  toggleDarkTheme() {
+    
+    const DarkModeBtn = document.getElementById("DarkThemeBtn");
+    const ContrastThemeBtn = document.getElementById("ContrastThemeBtn");
+    
+    document.body.classList.toggle('dark-theme');
+    
+    if (DarkModeBtn) {
+      if (DarkModeBtn.className === "active") 
+        {
+        DarkModeBtn.classList.remove("active");
+        DarkModeBtn.classList.add("disabled");
+        DarkModeBtn.innerHTML = "❌ Mode Sombre Désactivé";
+      }
+      else if (DarkModeBtn.className === "disabled"){
+        DarkModeBtn.classList.remove("disabled");
+        DarkModeBtn.classList.add("active");
+        DarkModeBtn.innerHTML = "✅ Mode Sombre Activé";
+
+        ContrastThemeBtn?.classList.add("disabled");
+        ContrastThemeBtn?.classList.remove("active");
+        if (ContrastThemeBtn) ContrastThemeBtn.innerHTML = "❌ Contrastes élevés désactivés";
+        document.body.classList.remove("high-contrast");
+      } 
+    }
+  }
+  
+  toggleHighContrast() {
+    const ContrastThemeBtn = document.getElementById("ContrastThemeBtn");
+    const DarkModeBtn = document.getElementById("DarkThemeBtn");
+    
+    document.body.classList.toggle('high-contrast');
+  
+      if (ContrastThemeBtn) {
+        if (ContrastThemeBtn.className === "active") 
+          {
+            ContrastThemeBtn.classList.remove("active");
+            ContrastThemeBtn.classList.add("disabled");
+            ContrastThemeBtn.innerHTML = "❌ Contrastes élevés désactivés";
+        }
+        else if (ContrastThemeBtn.className === "disabled"){
+          ContrastThemeBtn.classList.remove("disabled");
+          ContrastThemeBtn.classList.add("active");
+          ContrastThemeBtn.innerHTML = "✅ Contrastes élevés activés";
+
+          DarkModeBtn?.classList.remove("active");
+          DarkModeBtn?.classList.add("disabled");
+          if (DarkModeBtn) DarkModeBtn.innerHTML = "❌ Mode Sombre Désactivé";
+          document.body.classList.remove("dark-theme");
+        } 
+      }
+  }
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+  
+  ngOnInit() {
+    
+    // //Verify if the code is executed Client-Side and setup status of each buttons
+    if (isPlatformBrowser(this.platformId)) {
+      
+      //Init vars of elems of the DOM : high contrast & dark mode 
+      const DOMDarkModeBtn = document.getElementById("DarkThemeBtn");
+      const DOMContrastModeBtn = document.getElementById("ContrastThemeBtn");
+      
+      //Verify if the prefered color scheme OS is dark or light
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-theme');
+        this.isDarkModebtnActive = true;
+        DOMDarkModeBtn?.classList.add("active");
+        if (DOMDarkModeBtn) DOMDarkModeBtn.innerHTML = "✅ Mode sombre activé";
+        DOMContrastModeBtn?.classList.add("disabled");
+        if (DOMContrastModeBtn) DOMContrastModeBtn.innerHTML = "❌ Contrastes élevés désactivés";
+      } else {
+        //btns are disabled by default on first loading
+        document.getElementById("DarkThemeBtn")?.classList.add("disabled");
+        document.getElementById("ContrastThemeBtn")?.classList.add("disabled");
+      }
+    }
+  }
+  
 }
